6355228ba031d5bc176f0e5786e576902cc6ad2f
Ohrimus148 <Ohrimus@yandex.ru>
2017-07-22 00:21:49 +0300

    added a routes

---
+++ config/routes.php
@@ -0,0 +1,8 @@
+<?php
+return array(
+	
+        'user/login' => 'user/login',
+        'user/logout' => 'user/logout',
+        'cabinet' => 'cabinet/hello',
+        '' => 'user/register',
+    );
