b881181464d93593f1cf14027a1a27c11835ef63
Ohrimus148 <Ohrimus@yandex.ru>
2017-07-22 09:36:16 +0300

    files consist of views

---
+++ views/layouts/footer.php
@@ -0,0 +1,28 @@
+   <div class="page-buffer"></div>
+</div>
+
+<footer id="footer" class="page-footer"><!--Footer-->
+    <div class="footer-bottom">
+        <div class="container">
+            <div class="row">
+                <p class="pull-left"></p>
+                <p class="pull-right"></p>
+            </div>
+        </div>
+    </div>
+</footer><!--/Footer-->
+
+
+
+<script src="/template/js/jquery.js"></script>
+<script src="/template/js/jquery.cycle2.min.js"></script>
+<script src="/template/js/jquery.cycle2.carousel.min.js"></script>
+<script src="/template/js/bootstrap.min.js"></script>
+<script src="/template/js/jquery.scrollUp.min.js"></script>
+<script src="/template/js/price-range.js"></script>
+<script src="/template/js/jquery.prettyPhoto.js"></script>
+<script src="/template/js/main.js"></script>
+
+
+</body>
+</html>
